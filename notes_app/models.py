7a037982a54fadd5401c0cf518d7c39b2f31247f
apoorv127 <prasada@uwm.edu>
2020-10-29 14:01:51 -0500

    added comment in models.py

--- notes_app/models.py
+++ notes_app/models.py
@@ -15,7 +15,7 @@ class Diary(models.Model):
     note = models.CharField(max_length=50, blank=False)
     date_time_issued = models.DateTimeField(blank=False)
     day = models.CharField(max_length=50, blank=False)
-
+    # Category can have many Diary Entries, but a particular Diary Entry can have only one Category
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
